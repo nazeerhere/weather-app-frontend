3367eeadcdc7adc5d75a1e82352c4b9854dae1c4
nazeer171 <nazeer3886@gmail.com>
2021-08-17 09:17:11 -0500

    app now retrieves data and renders a basic display

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
 import LocationModal from "./LocationForm/LocationModal"
 
 export default function Home() {
-    
-    return(
-        <div className="Home">
-            <LocationModal />
-        </div>
-    )
+    const [Forcasts, setForcasts] = useState([])
+    console.log(Forcasts.length)
+    const arr = [
+        "something",
+        "honor",
+        "JID",
+        "more",
+    ]
+
+    function runMe() {
+        setForcasts([])
+    }
+
+    if (Forcasts.length <= 0) {
+        return(
+            <div className="Home">
+                <LocationModal  Forcasts={Forcasts} setForcasts={setForcasts} />
+            </div>
+        )
+    } else {
+
+        return(
+            <div className="Home">
+                <div className="weatherCard">
+                    {   
+                        Forcasts.map(obj => {
+                             
+                            return(
+                                <div className="unit">
+                                    {obj.date}
+                                </div>
+                            )
+                        })
+                    }
+                    <button onClick={runMe} >new City</button>
+                </div>
+                {/* <p style={{color: "green", fontSize: "20px"}}
+                > {Forcasts[0]} </p> */}
+                <p>J. cole</p>
+            </div>
+        )
+    }
 }
--- src/Components/LocationForm/LocationModal.jsx
+++ src/Components/LocationForm/LocationModal.jsx
@@ -1,14 +1,9 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import axios from "axios"
 import url from "../../BackendUrl"
 
-export default function LocationModal() {
-const initialState = {
-    Location: ""
-}
-
-    const [Location, setLocation] = useState(initialState)
-    const [data, setData] = useState({})
+export default function LocationModal({ Forcasts, setForcasts }) {
+    const [Location, setLocation] = useState({ Location: "" })
 
     const handleChange = (event) => {
         const {id, value} = event.target
@@ -26,21 +21,26 @@ const initialState = {
             `http://localhost:${url.value}/history`, 
             Location
         )
-            .then(res => {
-                if(res.data.length < 0) {
-                    setData(res.data)
-                    console.log(data)
-                    console.log("we got data!!")
-                } else {
-                    console.log("something went wrong")
-                }
-            })
+        .then(res => {
+            if(res.data.data.length > 0) {
+                let newArr = res.data.data.slice(0, 3)
+                setForcasts(newArr)
+                setTimeout(() => {
+                    console.log(Forcasts)
+                }, 500)
+                console.log(res.data.data[0])
+                console.log(res)
+                console.log("we got data!!")
+
+            } else {
+                console.log("something went wrong, Response: ")
+                console.log(res.data.data)
+            }
+        })
 
         document.getElementById("Location").innerHTML = ""
         console.log("HELLO FROM THE OTHER SIDE")
         console.log(Location.Location)
-
-        // document.location.href = `${Location.location}-weather`
     }
 
     return(
@@ -53,7 +53,7 @@ const initialState = {
                         id="Location"
                         onChange={handleChange}
                         required
-                        />
+                />
                 <button
                     onClick={handleSubmit}
                 >Do something</button>
